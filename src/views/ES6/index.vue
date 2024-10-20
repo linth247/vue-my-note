85c38b2cbac76f7fd8c991f24882acc64509f39b
linth247 <lin.th247@gmail.com>
2024-10-20 22:16:59 +0800

    20241020-01

--- src/views/ES6/index.vue
+++ src/views/ES6/index.vue
@@ -9,14 +9,348 @@ const content = `
       <a href="https://www.youtube.com/playlist?list=PLmOn9nNkQxJFlj86lvBSpC2UsNw-pdmdq" target="_blank">
         【尚硅谷】ES6教程——涵盖ES6-ES11
       </a>
+      <a href="https://www.youtube.com/watch?v=Em-vw0xLbRg&list=PLmOn9nNkQxJFlj86lvBSpC2UsNw-pdmdq&index=4" target="_blank">
+        【尚硅谷】04 ES6 let經典案例實踐
+      </a>
+      <"div class="container">
+        <"div class="page-header">點擊切換顏色<"/div>
+        <"div class="item"><"/div>
+        <"div class="item"><"/div>
+        <"div class="item"><"/div>
+      <"/div>
+      <"scrip>
+        let item = document.getElementByClassName('item');
 
-      //================== 18 尚硅谷 ES6 迭代器介绍
+        for(let i = 0; i < items.length; i++){
+          items[i].onclick = function(){
+            items[i].style.background = 'pink'
+          }
+        }
+      <"/scipt>
+
+      <a href="https://www.youtube.com/watch?v=1zI1G6edZLI&list=PLmOn9nNkQxJFlj86lvBSpC2UsNw-pdmdq&index=10" target="_blank">
+        【尚硅谷】10 ES6 箭頭函數的實踐與應用場景
+      </a>
+      <"div id="ad"><"/div>
+      <"scrip>
+        // 需求-1 點擊 div 2s 後顏色變成「粉色」
+        // 獲取元素
+        let ad = document.getElementById('ad');
+        // 綁定事件
+        ad.addEventListener("click", function(){
+          //保存 this 的值
+          // let _this = this;
+          setTimeout(() => {
+            // 修改背景顏色 this
+            this.style.background = 'pink';
+          }, 2000);
+        })
+
+        // 需求-2 從數組中返回偶數的元素
+        const arr = [1,6,9,10,100,25]
+        // const result = arr.filter(item => {
+        //   if(item % 2 === 0){
+        //     return true;
+        //   }else{
+        //     return false;
+        //   }
+        // })
+        const result = arr.filter(item => item % 2 === 0)
+        console.log(result);
+
+        // 箭頭函數適合與 this 無關的回調，定時器，數組的方法回調
+        // 箭頭函數不適合 與 this 有關的回調，事件回調，對象的方法
+
+        {
+          name: '尚硅谷',
+          getName: () => {
+            this.name; // 這裡的name是無法取到'尚硅谷'
+          }
+        }
+      <"/scipt>
+
+      <a href="https://www.youtube.com/watch?v=X0jxwLC6dyI&list=PLmOn9nNkQxJFlj86lvBSpC2UsNw-pdmdq&index=11" target="_blank">
+        【尚硅谷】11 ES6 函數參數的默認值設置
+      </a>
+      <"scrip>
+        //ES6 允許給函數參數賦值初始值
+        //1.形參初始值 具有默認值的參數，一般位置要靠後
+        function add(a,b,c=10){
+          return a + b + c;
+        }
+        let restul = add(1,2);
+        console.log(result);
+
+        //2. 與解構賦值結合
+        function connect({host="127.0.0.1", username, password, port}){
+          console.log(host)
+          console.log(username)
+          console.log(password)
+          console.log(port)
+        }
+        connect({
+          host: 'atguigu.com',
+          username: 'root',
+          password: 'root',
+          port: 3306
+        })
+      <"/scrip>
+
+      <a href="https://www.youtube.com/watch?v=wEEtFdMEih0&list=PLmOn9nNkQxJFlj86lvBSpC2UsNw-pdmdq&index=12" target="_blank">
+        【尚硅谷】12 ES6 rest參數
+      </a>
+      <"scrip>
+        // ES6 引入 rest 參數, 用於獲取函數的實參，用來代替 arguments
+        // ES6 獲取實參的方式
+        // function date(){
+        //   console.log(arguments);  // 對象
+        // }
+        // date('白芷', '阿嬌', '思慧');
+
+        // rest 參數
+        function date(...args) {{
+          console.log(args); // 數組，可以用filter, some, every, map方法
+        }}
+        date('白芷', '阿嬌', '思慧');
+
+        // rest 參數必須要放到參數最後
+        function fn(a,b,...args){
+          console.log(a);
+          console.log(b);
+          console.log(args); // [3,4,5,6]
+        }
+        fn(1,2,3,4,5,6);
+      <"/scrip>
+
+      <a href="https://www.youtube.com/watch?v=aisJZnNztJU&list=PLmOn9nNkQxJFlj86lvBSpC2UsNw-pdmdq&index=13" target="_blank">
+        【尚硅谷】13 ES6 擴展運算符的介紹
+      </a>
+      // [...] 擴展運算符能將「數組」轉換為逗號分隔的「參數序列」
+      // 聲明一個數組
+      const tfboys = ['易陽千璽','王源','王俊凱'] // =>'易陽千璽','王源','王俊凱'
+      
+      //聲明一個函數
+      function chunwan(){
+        console.log(arguments)
+      }
+      chunwan(...tfboys); // chunwan('易陽千璽','王源','王俊凱')
+      <a href="https://www.youtube.com/watch?v=zp_mKn344Oo&list=PLmOn9nNkQxJFlj86lvBSpC2UsNw-pdmdq&index=14" target="_blank">
+        【尚硅谷】14 ES6 擴展運算符的應用
+      </a>
+      // 1.數組的合併 情聖 誤殺 唐探
+      const kuaizi = ['王太利','肖央']
+      const fenghuang = ['曾毅','玲花']
+      // const zuixuanxianpingguo = kuaizi.concat(fenghuang);
+      const zuixuanxianpingguo = [...kuaizi, kuaizi];
+                                 //['王太利','肖央','曾毅','玲花']
+      console.log(zuixuanxianpingguo);
+
+      // 2.數組的克隆
+      const sanzhihua = ['E','G','M'];
+      const sanyecao = [...sanzhihua]; // ['E','G','M'] 如果是物件，是淺拷貝
+      console.log(sanyecao);
+
+      // 3.將偽數組轉為直正的數組
+      const divs = document.querySelectAll('div');
+      // console.log(divs);
+      const divArr = [...divs];
+      console.log(divArr);
+
+      <a href="https://www.youtube.com/watch?v=lwxrvInKas8&list=PLmOn9nNkQxJFlj86lvBSpC2UsNw-pdmdq&index=15" target="_blank">
+        【尚硅谷】15 ES6 Symbol的介紹與創建
+      </a>
+      ES6引入了一種新的原始數據類型Symbol。表示獨一無二的值。它是Javascript語言的第七種
+      數據類型，是一種類似於字符中的數據類型。
+
+      Symbol特點
+      1.值是唯一的，用來解決命名衝突的問題
+      2.值不能與其他數據進行運算
+      3.定義的對象屬性不能使用 for...in 循環遍歷，但是可以使用Reflect.ownKeys來獲取對象的所有鍵名
+
+      // 創建Symbol
+      let s1 = Symbol();
+      console.log(s1, typeof s1);
+
+      let s2 = Symbol('尚硅谷');
+      let s3 = Symbol('尚硅谷');
+      console.log(s2 === s3); // false
+
+      //Symbol.for 創建
+      let s4 = Symbol.for('尚硅谷');
+      let s5 = Symbol.for('尚硅谷');
+      console.log(s4, typeof s4);
+      console.log(s4 === s5); // true
+
+      // 不能與其他數據進行運算
+      // let result = s1 + 100;
+      // let result = s1 > 100;
+      // let result = s1 + s1
+
+      // USONB you are so niubility
+      // u undefined
+      // s string
+      // o object
+      // n null number
+      // b boolean
+
+      <a href="https://www.youtube.com/watch?v=lwxrvInKas8&list=PLmOn9nNkQxJFlj86lvBSpC2UsNw-pdmdq&index=16" target="_blank">
+        【尚硅谷】16 ES6 對象添加Symbol類型的屬性
+      </a>
+        // 向對象中添加方法 up down
+        let game = { name:'俄羅斯方塊' }
+
+        // 聲明一個對象
+        let methods = { 
+          up: Symbol(),
+          down: Symbol()
+        }
+        
+        game[methods.up] = function(){
+          console.log("我可以改變形狀");
+        }
+
+        game[methods.down] = function(){
+          console.log("我可以快速下降");
+        }
+
+        console.log(game)
+
+        // 
+        let youxi = {
+          name:"狼人殺"
+          [Symbol('say')]: function(){
+            console.log('我可以發言')
+          },
+          [Symbol('sibao')]: function(){
+            console.log('我可以自爆')
+          }
+        }
+        console.log(youxi)
+
+      <a href="https://www.youtube.com/watch?v=KAQNkgFByBU&list=PLmOn9nNkQxJFlj86lvBSpC2UsNw-pdmdq&index=17" target="_blank">
+        【尚硅谷】17 ES6 Symbol的内置属性
+      </a>
+      class Person{
+        static [Symbol.hasInstance(param){
+          // 可以自行控制類型檢測
+          console.log(param)
+          console.log("我被用來檢測類型了")
+          return false;
+        }]
+      }
+      let o = {};
+      console.log(o instanceof Person);
+
+      //
+      const arr = [1,2,3]
+      const arr2 = [4,5,6]
+      arr2[Symbol.isConcatSpreadable] = false; // 表示應對象是否可以展開
+      console.log(arr.concat(arr2)); 
+
+
+      <a href="https://www.youtube.com/watch?v=KAQNkgFByBU&list=PLmOn9nNkQxJFlj86lvBSpC2UsNw-pdmdq&index=18" target="_blank">
+        【尚硅谷】18 尚硅谷 ES6 迭代器介绍
+      </a>
+      1.ES6創造了一種新的遍歷命令for...of循環，Iterator接口主要供for...of消費
+      2.原生具備iterator接口的數據(可用for of遍歷)
+        a.Array
+        b.Arguments
+        c.Set
+        d.Map
+        e.String
+        f.TypeArray
+        g.NodeList
+      3.工作原理
+        a.創建一個指針對象，指向當前數據結構的起始位置
+        b.第一次調用對象的next方法，指針自動指向數據結構的第一個成員
+        c.接下來不斷調用next方法，指針一直往後移動，直接指向最後一個成員
+        d.每調用next方法返回一個包含value和done屬性的對象
+
+      const xiyou = ['唐憎','孫悟空','豬八戒','沙憎']
+
       for(let v in xiyou){  // for in 保存的是鍵名
         console.log(v)
       } 
       for(let v of xiyou){  // for in 保存的是鍵值
         console.log(v)
       } 
+
+      let iterator = xiyou[Symbol.iterator()];
+      //調用對象的next方法
+      console.log(iterator.next());
+      console.log(iterator.next());
+      console.log(iterator.next());
+      console.log(iterator.next());
+      console.log(iterator.next());
+
+      <a href="https://www.youtube.com/watch?v=KAQNkgFByBU&list=PLmOn9nNkQxJFlj86lvBSpC2UsNw-pdmdq&index=18" target="_blank">
+        【尚硅谷】19 尚硅谷 ES6 迭代器应用 自定义遍历数据
+      </a>
+      //迭代器就是要自定義遍歷數據
+      // 聲明一個對象
+      const banji = {
+        name:"終極一班",
+        stus:[
+          'xiaoming',
+          'xiaoning',
+          'xiaotian',
+          'knight',
+        ],
+        [Symbol.iterator](){
+          //索引變量
+          let index = 0;
+          let _this = this;
+          return {
+            next: function(){
+              if(index < _this.stus.length){
+                const result = {value: _this.stus[index], done: false} // 會瘋狂輸出
+                index++;
+                return result;
+              }else{
+                return {value: undefined, done: true}
+              }
+            }
+          };
+        }
+      }
+
+      // 遍歷這個對象
+      for(let v of banji){
+        console.log(v);
+      }
+      
+      // banji.stus.forEach()
+      <a href="https://www.youtube.com/watch?v=KAQNkgFByBU&list=PLmOn9nNkQxJFlj86lvBSpC2UsNw-pdmdq&index=18" target="_blank">
+        【尚硅谷】20 尚硅谷 ES6 生成器函数声明与调用
+      </a>
+      // 生成器就是一個特殊函數，是ES6提供的一種異步編程解決方案，語法行為與傳統函數完全不同。
+      // 異步編程： 純口調函數 node fs ajax mongodb
+      // yield：函數代碼的分隔符
+      function *gen(){
+        console.log("hello generator")
+        console.log("111");
+        yield '一隻沒有耳朵'
+        console.log("222");
+        yield '一隻沒有尾巴'
+        console.log("333");
+        yield '真奇怪'
+        console.log("444");
+      }
+      let iterator = gen();
+      // console.log(iterator);
+      console.log(ietrator.next()); // 這裡才會執行
+      console.log(ietrator.next()); // 這裡才會執行
+      console.log(ietrator.next()); // 這裡才會執行
+      console.log(ietrator.next()); // 這裡才會執行
+
+      //遍歷
+      for(let v of gen()){
+        console.log(v)
+      }
+
+      <a href="https://www.youtube.com/watch?v=KAQNkgFByBU&list=PLmOn9nNkQxJFlj86lvBSpC2UsNw-pdmdq&index=18" target="_blank">
+        【尚硅谷】21 尚硅谷 ES6 生成器函数的参数传递
+      </a>
 
       // 48.Function Factories (JavaScript全攻略)
       function maeGreeting(language){
